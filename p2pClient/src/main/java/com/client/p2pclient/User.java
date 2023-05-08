867cb89e04d0f8ae1398119afc6ed3481ba9b612
RihamKatout <54861182+RihamKatout@users.noreply.github.com>
2023-05-08 12:44:11 +0300

    Hello World!

--- p2pClient/src/main/java/com/client/p2pclient/User.java
+++ p2pClient/src/main/java/com/client/p2pclient/User.java
@@ -8,30 +8,27 @@ import java.net.UnknownHostException;
 
 public class User {
     private String username, IP, TCPServerIP;
-    private UDPServerThread UDPserver ;
+    private UDPServerThread UDPServer;
     private int onlineServerPort, port;
     private onlineStatusThread onlineThread ;
 
     User() throws SocketException, UnknownHostException {
         IP = username = null;
+        UDPServer = null;
         port = 0 ;
     }
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getUsername() {
         return username;
     }
-
     public void setIP(String IP) {
         this.IP = IP;
     }
-
     public String getIP() {
         return IP;
     }
-
     public void setPort() throws IOException {
         for(int ports = 1 ; ports <= 9999; ports++){
             try {
@@ -51,28 +48,25 @@ public class User {
     public void setTCPServerIP(String TCPServerIP) {
         this.TCPServerIP = TCPServerIP;
     }
-
+    public void createUDPThread() throws IOException {
+        if(UDPServer != null && !UDPServer.getSocket().isClosed())
+            UDPServer.getSocket().close();
+        UDPServer = new UDPServerThread();
+        setPort();
+        UDPServer.setPort(port);
+        UDPServer.getSocket().connect(InetAddress.getByName((InetAddress.getLocalHost().getHostAddress())),port);
+        System.out.println("UDP created successfully");
+    }
+    public UDPServerThread getUDPServer(){
+        return UDPServer;
+    }
+    ////////////////////////////////////////////////
     public void setOnlineServerPort(int onlineServerPort) {
         this.onlineServerPort = onlineServerPort;
     }
-
     public int getOnlineServerPort() {
         return onlineServerPort;
     }
-
-    public void createUDPThraed() throws IOException {
-        if(UDPserver != null && !UDPserver.getSocket().isClosed())
-            UDPserver.getSocket().close();
-        UDPserver = new UDPServerThread();
-        setPort();
-        UDPserver.setPort(port);
-        UDPserver.getSocket().connect(InetAddress.getByName((InetAddress.getLocalHost().getHostAddress())),port);
-        System.out.println("UDP created successfully");
-    }
-    public UDPServerThread getUDPserver(){
-        return UDPserver;
-    }
-
     public boolean equals(User user2) {
         return IP.equals(user2.getIP()) && port==(user2.getPort());
     }
